a1ebac0b645b64710184812e2a4fa166253c7452
Dimgusar <dimgusar@mail.ru>
2024-06-26 18:35:56 +0300

    зарождение деревни

---
+++ src/main/java/org/example/Village.java
@@ -0,0 +1,9 @@
+public class Village {
+
+    final int level;
+    boolean destroyed = false;
+    int slaves;
+    final VillageType theType;
+    final int longitude;
+    final int lattitude;
+    TreeMap<LootType, Integer> all_loot;v
